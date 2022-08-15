63bd9937c23f3b90c942c614043778e43a25f7c5
Arthur <peraarthur2018@gmail.com>
2022-08-15 11:37:47 -0300

    pomodor timer

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,6 +1,24 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  flex: 1;
 
-export const FormContainer = styled.div``
-export const CountdownContainer = styled.div``
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+
+`
+
+export const CountdownContainer = styled.div`
+`
