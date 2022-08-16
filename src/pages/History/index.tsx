d3a2094df89bb80edeb0cd5377753243370c6c03
Arthur <peraarthur2018@gmail.com>
2022-08-16 16:43:01 -0300

    History update

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,77 @@
-import { HistoryContainer } from './style'
+import { HistoryContainer, HistoryList } from './style'
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>My History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>ínicio</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+              <td>tarefa</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </HistoryContainer>
   )
 }
